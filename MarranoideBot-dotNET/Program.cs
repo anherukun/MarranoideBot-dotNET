b0c3b3c1eede9ae2fba70396487af84657948431
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2022-05-01 12:15:24 -0500

    Agregar archivos de proyecto.

---
+++ MarranoideBot-dotNET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarranoideBot_dotNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
