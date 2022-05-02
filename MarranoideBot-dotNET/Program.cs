4f18bcb75ee887013d677e33a2fbeec48b2f5601
Angel Gerardo Jimenez Reyes <angel.g.j.reyes@outlook.com>
2022-05-01 19:06:59 -0500

    Slashcommands

--- MarranoideBot-dotNET/Program.cs
+++ MarranoideBot-dotNET/Program.cs
@@ -1,12 +1,100 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using Discord.Net;
+using Discord.WebSocket;
+using MarranoideBot_dotNET.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace MarranoideBot_dotNET
 {
     class Program
     {
-        static void Main(string[] args)
+        private readonly IConfiguration _config;
+        private DiscordSocketClient _client;
+        private InteractionService _commands;
+        private ulong _testGuildId;
+        
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public Program()
         {
-            Console.WriteLine("Hello World!");
+            var _builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(path: "config.json");
+            // 
+            _config = _builder.Build();
+            _testGuildId = ulong.Parse(_config["TestGuildId"]);
+        }
+
+
+        public async Task MainAsync()
+        {
+            // Console.WriteLine("Hola Mundo!!");
+
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
+                var commands = services.GetRequiredService<InteractionService>();
+                _client = client;
+                _commands = commands;
+
+                // setup logging and the ready event
+                client.Log += Log;
+                commands.Log += Log;
+                client.Ready += Client_Ready;
+
+                // new LoggingService(_client, commands);
+
+                var token = Configuration.BOTTOKEN;
+
+                Console.WriteLine($"Conectando bot");
+
+                await client.LoginAsync(TokenType.Bot, token);
+                await client.StartAsync();
+
+                await services.GetRequiredService<CommandHandler>().InitializeAsync();
+
+                await Task.Delay(-1);
+
+            }
+        }
+
+        public async Task Client_Ready()
+        {
+            try
+            {
+                // await _commands.RegisterCommandsToGuildAsync(Configuration.GUILDID);
+
+                await _commands.RegisterCommandsGloballyAsync(true);
+            }
+            catch (HttpException exception)
+            {
+                // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+
+                // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+                Console.WriteLine(json);
+            }
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton(_config)
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<CommandHandler>()
+                .BuildServiceProvider();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
---
+++ MarranoideBot-dotNET/SlashCommands/InteractionCommands.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using MarranoideBot_dotNET.Services;
+
+namespace MarranoideBot_dotNET.SlashCommands
+{
+    public class InteractionCommands : InteractionModuleBase<SocketInteractionContext>
+    {
+        public InteractionService Commands { get; set; }
+        private CommandHandler _handler;
+
+        public InteractionCommands(CommandHandler handler)
+        {
+            _handler = handler;
+        }
+        // public InteractionCommands(InteractionService commands, CommandHandler handler)
+        // {
+        //     Commands = commands;
+        //     _handler = handler;
+        // }
+
+        [SlashCommand("ping", "Pong!")]
+        public async Task HandlePing()
+        {
+            var embed = new EmbedBuilder()
+                .WithTitle("🏓 Pong")
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp();
+
+            await RespondAsync(embed: embed.Build());
+        }
+
+        [SlashCommand("8ball", "Conoce tu suerte!")]
+        public async Task HandleEightBall(string question)
+        {
+            // create a list of possible replies
+            var replies = new List<string>();
+
+            // add our possible replies
+            replies.Add("Si");
+            replies.Add("No");
+            replies.Add("Tal vez");
+            replies.Add("Intentalo mas tarde....");
+            replies.Add("Honestamente no me importa jaja lol");
+            replies.Add("Ve y preguntale a tus amiguitas");
+            replies.Add("Si ya, ya... no estes chi***ndo");
+
+            // get the answer
+            var answer = replies[new Random().Next(replies.Count - 1)];
+
+            var embed = new EmbedBuilder()
+                .WithTitle($"🎱 {question}")
+                .WithDescription($"**{answer}**")
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp();
+
+            await RespondAsync(embed: embed.Build());
+        }
+
+        [SlashCommand("time", "Hora del servidor")]
+        public async Task HandleServerTime()
+        {
+            var s = $"{DateTime.Now.ToLongDateString()} @ {DateTime.Now.ToString("HH:MM:ss")}";
+            var embed = new EmbedBuilder()
+                .WithTitle("🕓 La hora del bot es")
+                .WithDescription($"{s}")
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp();
+
+            await RespondAsync(embed: embed.Build());
+        }
+
+        [SlashCommand("latency", "Mide el tiempo de respuesta del servidor")]
+        public async Task HandleServerLatency()
+        {
+            Ping mPing = new Ping();
+            string pDNS = "";
+            string pDiscord = "";
+
+
+            PingReply reply = mPing.Send("1.1.1.1", 1000);
+            if (reply != null)
+            {
+                if (reply.RoundtripTime < 100)
+                    pDNS = $"El tiempo de respuesta del servidor al DNS es: {reply.RoundtripTime}ms 🟢";
+
+                else if (reply.RoundtripTime < 130)
+                    pDNS = $"El tiempo de respuesta del servidor al DNS es: {reply.RoundtripTime}ms 🟡";
+
+                else if (reply.RoundtripTime >= 130)
+                    pDNS = $"El tiempo de respuesta del servidor al DNS es: {reply.RoundtripTime}ms 🔴";
+
+            }
+
+            reply = mPing.Send("discord.com", 1000);
+            if (reply != null)
+            {
+                if (reply.RoundtripTime < 100)
+                    pDiscord = $"El tiempo de respuesta del servidor a Discord es: {reply.RoundtripTime}ms 🟢";
+
+                else if (reply.RoundtripTime < 130)
+                    pDiscord = $"El tiempo de respuesta del servidor a Discord es: {reply.RoundtripTime}ms 🟡";
+
+                else if (reply.RoundtripTime >= 130)
+                    pDiscord = $"El tiempo de respuesta del servidor a Discord es: {reply.RoundtripTime}ms 🔴";
+
+            }
+
+            var embed = new EmbedBuilder()
+                .WithTitle("🌐 Latencia del bot")
+                .WithDescription($"{pDNS} \n{pDiscord}")
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp();
+
+            await RespondAsync(embed: embed.Build());
+        }
+    }
+}
